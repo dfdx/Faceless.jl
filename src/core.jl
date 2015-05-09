f38e99c27e2a12547d075b5ed1f2f8cebfc1b4a6
dfdx <faithlessfriend@gmail.com>
2015-05-10 02:12:08 +0300

    successful experiments on RBMs on appearance

--- src/core.jl
+++ src/core.jl
@@ -12,8 +12,6 @@ using HDF5, JLD
 
 include("view.jl")
 
-# 4. apply PCA
-# 5. apply RBM
 
 const DATA_DIR_CK = expanduser("~/work/ck-data")
 
@@ -70,26 +68,84 @@ nview(img) = view(normalize(img))
 
 function save_images{T,N}(imgs::Vector{Array{T,N}}, path::String; prefix="img")
     for i=1:length(imgs)
-        imwrite(imgs[i], joinpath(path, @sprintf("%s_%03d.png", prefix, i)))
+        img = normalize(imgs[i])
+        imwrite(img, joinpath(path, @sprintf("%s_%03d.png", prefix, i)))
     end
 end
 
 
 function main()
-    # imgs = read_images_ck(DATA_DIR_CK, resizeratio=0.5)
-    # shapes = read_shapes_ck(DATA_DIR_CK, resizeratio=0.5)
-    # @time aam = train(AAModel(), imgs, shapes)
+    imgs = imgs = load_images(:ck, datadir=DATA_DIR_CK, resizeratio=0.5)
+    shapes = load_shapes(:ck, datadir=DATA_DIR_CK, resizeratio=0.5)
+    ## @time aam = train(AAModel(), imgs, shapes)
 
-    imgs = load_images(:ck, DATA_DIR_CK, count=300, resizeratio=0.5)
-    shapes = load_images(:ck, DATA_DIR_CK, count=300, resizeratio=0.5)
-    @time aam = load(joinpath(DATA_DIR_CK, "aam.jld"))["aam"]
+    ## indexes = rand(1:10708, 2048)
+    ## imgs = load_images(:ck, datadir=DATA_DIR_CK, indexes=indexes, resizeratio=0.5)
+    ## shapes = load_shapes(:ck, datadir=DATA_DIR_CK, indexes=indexes,
+    ##                      resizeratio=0.5)
+    @time aam = load(joinpath(DATA_DIR_CK, "aam.jld"))["aam"]    
     
     dataset = to_dataset(aam, imgs, shapes)
+    mask = aam.wparams.warp_map
+    
+    rbm = GRBM(16177, 1024, sigma=0.001)
+    @time fit(rbm, dataset, n_gibbs=3, lr=0.1, n_iter=3)
+
+
     P = projection(fit(PCA, dataset))
     nview(to_image(P[:, 1], aam.wparams.warp_map))
     pca_imgs = Matrix{Float64}[normalize(to_image(P[:, i], aam.wparams.warp_map))
                                for i=1:size(P, 2)]
     save_images(pca_imgs,
                 expanduser("~/Dropbox/PhD/MyPapers/facial_expr_repr/images/pca"))
+
+    
+    comps = components(rbm)
+    rbm_imgs = [to_image(comps[:, i], mask) for i=1:size(comps, 2)]
+    nviewall(rbm_imgs[1:36])
+
+    save_images(convert(Vector{Matrix{Float64}}, rbm_imgs),
+                expanduser("~/Dropbox/PhD/MyPapers/facial_expr_repr/images/rbm"))
+    
+    h5open(joinpath(DATA_DIR_CK, "rbm_1_9591.h5"), "w") do h5
+        save_params(h5, rbm, "rbm")
+    end
+
+    ## h5open(joinpath(DATA_DIR_CK, "rbm_1.jld")) do h5
+    ##     rbm2 = save_params(h5, rbm, "rbm")
+    ## end
+    
 end
 
+
+
+# some results from previous experiments
+# n_hid=192, n_gibbs=3, n_meta=10 ~ 7 faces of good quality
+# n_hid=192, n_gibbs=5, n_meta=10 ~ 4 faces of good quality
+# n_hid=256, n_gibbs=1, n_meta=10 ~ no faces at all
+# n_hid=256, n_gibbs=3, n_meta=10 ~ 10 faces of almost good quality
+# n_hid=256, n_gibbs=5, n_meta=10 ~ 11 faces of almost good quality
+# n_hid=256, n_gibbs=10, n_meta=10 ~ 11 faces of almost good quality
+# n_hid=256, n_gibbs=5, n_meta=20 ~ 12 faces of almost good quality
+# n_hid=128, n_gibbs=3, n_meta=10 ~ all faces of bad quality
+# n_hid=192, n_gibbs=3, n_meta=10 ~ 3 faces of a modate quality
+# n_hid=256, n_gibbs=3, n_meta=10 ~ 10 faces of moderate quality
+# n_hid=320, n_gibbs=3, n_meta=10 ~ 16 faces of almost good qualiy
+# n_hid=384, n_gibbs=3, n_meta=10 ~ 17 faces of almost good qualiy
+# n_hid=512, n_gibbs=3, n_meta=10 ~ 22 faces of different quality
+# n_hid=768, n_gibbs=3, n_meta=10 ~ 25 faces of different quality
+# n_hid=1024, n_gibbs=3, n_meta=10 ~ 21 face of different quality
+# n_hid=768, n_gibbs=3, n_meta=10, sigma=0.001 ~ almost all faces!
+# n_hid=512, n_gibbs=3, n_meta=10, sigma=0.001 ~ all good, but many similar
+# n_hid=1024, n_gibbs=3, n_meta=10, sigma=0.001 ~ all good
+# n_hid=128, n_gibbs=3, n_meta=10, lr=0.01 ~ 1 good, others similar and bad
+# n_hid=128, n_gibbs=10, n_meta=10, lr=0.01 ~ 1 good, others similar and bad
+# brbm: n_hid=768, n_gibbs=3, n_meta=10, lr=0.01 ~ 20 good
+# grbm: n_hid=1024, n_gibbs=3, n_meta=10 ~ half almost good
+# grbm: n_hid=768, n_gibbs=3, n_meta=10 ~ half almost good
+# grbm: n_hid=1024, n_gibbs=10, n_meta=10 ~ half almost good
+# grbm: n_hid=1024, n_gibbs=10, n_meta=3, lr=0.01 ~ half almost good
+#! grbm: n_hid=1024, n_gibbs=3, n_meta=10, sigma=0.001, lr=0.01 ~ all really good!
+# brbm: n_hid=1024, n_gibbs=3, n_meta=10, sigma=0.001, lr=0.01 ~ almost the same
+
+# grbm (w/ momentum): n_hid=1024, n_gibbs=2, n_meta=5, sigma=0.001, lr=0.01 ~ 2243
--- src/view.jl
+++ src/view.jl
@@ -8,3 +8,27 @@ function viewshape(img::Image, lms::Matrix{Float64})
     imgc, img2
 end
 viewshape(mat::Matrix{Float64}, lms::Shape) = viewshape(convert(Image, mat), lms)
+
+
+function nviewall(imgs, padding=10)
+    @assert(length(imgs) > 0, "Need at least one image")
+    h, w = size(imgs[1])
+    n = length(imgs)
+    rows = int(floor(sqrt(n)))
+    cols = int(ceil(n / rows))
+    halfpad = div(padding, 2)
+    dat = zeros(rows * (h + padding), cols * (w + padding))
+    for i=1:n
+        ## wt = W[i, :]
+        ## wt = reshape(wt, length(wt))
+        ## wim = map_nonzeros(IMSIZE, wt, nzs)
+        img = imgs[i]
+        img = img ./ (maximum(img) - minimum(img))
+        r = div(i - 1, cols) + 1
+        c = rem(i - 1, cols) + 1
+        dat[(r-1)*(h+padding)+halfpad+1 : r*(h+padding)-halfpad,
+            (c-1)*(w+padding)+halfpad+1 : c*(w+padding)-halfpad] = imgs[i]
+    end
+    nview(dat)
+    return dat
+end
